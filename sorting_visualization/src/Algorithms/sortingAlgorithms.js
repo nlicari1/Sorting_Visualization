a387afda8dd9dd8700b28e6a5a5898b30ec5a7b6
Nick <81262895+nlicari1@users.noreply.github.com>
2024-09-27 19:51:48 -0400

    fixed merge sort, added animation

--- sorting_visualization/src/Algorithms/sortingAlgorithms.js
+++ sorting_visualization/src/Algorithms/sortingAlgorithms.js
@@ -1,19 +1,67 @@
-export const mergeSort = array => {
-    if (array.length === 1) return array;
-    const middleIndex = Math.floor(array.length / 2)
-    const firstHalf = mergeSort(array.slice(0, middleIndex))
-    const secondHalf = mergeSort(array.slice(middleIndex))
-    const sortedArray = []
-    
-    let i = 0, j = 0;
-    while (i < firstHalf.length && j < secondHalf.length) {
-        if(firstHalf[i] < secondHalf[j]) {
-            sortedArray.push(firstHalf[i++])
+export function getMergeSortAnimations(array){
+    const animations = []
+    if (array.length <= 1) return array
+    const auxiliaryArray = array.slice()
+    mergeSortHelper(array, 0, array.length - 1, auxiliaryArray, animations)
+    return animations;
+}
+function mergeSortHelper( 
+    mainArray,
+    startIndex,
+    endIndex,
+    auxiliaryArray,
+    animations,
+) {
+    if(startIndex === endIndex) return;
+    const middleIndex = Math.floor((startIndex + endIndex) / 2);
+    mergeSortHelper(auxiliaryArray, startIndex, middleIndex, mainArray, animations)
+    mergeSortHelper(auxiliaryArray, middleIndex + 1, endIndex, mainArray, animations)
+    doMerge(mainArray, startIndex, middleIndex, endIndex, auxiliaryArray, animations)
+}
+
+
+function doMerge(
+    mainArray,
+    startIndex,
+    middleIndex,
+    endIndex,
+    auxiliaryArray,
+    animations,
+) {
+    let k = startIndex
+    let i = startIndex
+    let j = middleIndex + 1
+    while (i <= middleIndex && j <= endIndex){
+        //values being compared, pushed once to change color
+        animations.push([i,j])
+        // values being compared then pushed second time to revert color
+        animations.push([i,j])
+        if(auxiliaryArray[i] <= auxiliaryArray[j]) {
+            //overwrite the value at index k in original array with value at index i from auxiliary array
+            animations.push([k, auxiliaryArray[i]])
+            mainArray[k++] = auxiliaryArray[i++]
         } else {
-            sortedArray.push(secondHalf[j++])
+            //overwrite the value at index k in original array with value at index j from auxiliary array
+            animations.push([k, auxiliaryArray[j]])
+            mainArray[k++] = auxiliaryArray[j++]
         }
     }
-    while (i < firstHalf.length) sortedArray.push(firstHalf[i++])
-    while (j < secondHalf.length) sortedArray.push(secondHalf[j++])
-    return sortedArray
-};
+    while (i <= middleIndex){
+        //these values that we are comparing push them to change color
+        animations.push([i,i])
+        //these values that we are comparing push them a second time to revert color
+        animations.push([i,i])
+        //overwrite the value at index k in original array with value at index i from auxiliary array
+        animations.push([k,auxiliaryArray[i]])
+        mainArray[k++] = auxiliaryArray[i++]
+    }
+    while (j <= endIndex){
+        //these values that we are comparing push them to change color
+        animations.push([j,j])
+        //these values that we are comparing push them a second time to revert color
+        animations.push([j,j])
+        //overwrite the value at index k in original array with value at index j from auxiliary array
+        animations.push([k,auxiliaryArray[j]])
+        mainArray[k++] = auxiliaryArray[j++]
+    }
+}
--- sorting_visualization/src/SortingVisualizer/SortingVisualizer.jsx
+++ sorting_visualization/src/SortingVisualizer/SortingVisualizer.jsx
@@ -1,6 +1,13 @@
 import React from "react";
-import * as sortingAlgorithms from '../Algorithms/sortingAlgorithms.js';
+import {getMergeSortAnimations} from '../Algorithms/sortingAlgorithms.js';
 import './SortingVisualizer.css';
+
+//value of speed for animations
+const ANIMATION_SPEED_MS = 3;
+
+// value for number of bars in array
+const NUMBER_OF_ARRAY_BARS = 310;
+
 
 export default class SortingVisualizer extends React.Component {
     constructor(props) {
@@ -13,24 +20,42 @@ export default class SortingVisualizer extends React.Component {
     }   
  
 
- /* when app loads resets array */
+ // when app loads resets array 
  componentDidMount() {
     this.resetArray();
  }
 
-  /* creates array up too 100 between 5-1000 then resets state of array */
+  // creates array up too 100 between 5-1000 then resets state of array 
  resetArray() {
     const array = [];
-    for (let i = 0; i < 100; i++){
+    for (let i = 0; i < NUMBER_OF_ARRAY_BARS; i++){
         array.push(randomIntFromInterval(5, 1000));
     }
     this.setState({array});
  } 
  mergeSort() { 
-    const javaScriptSortedArray = this.state.array.slice() /*creates copy of main array */
-    .sort((a,b) => a - b); /* sorting method for js */
-    const sortedArray = sortingAlgorithms.mergeSort(this.state.array); 
-    console.log(arraysAreEqual(javaScriptSortedArray, sortedArray));
+   const animations = getMergeSortAnimations(this.state.array) // get all merge sort animations
+   for (let i = 0; i<animations.length;i++){ //iterate through animations
+    const arrayBars = document.getElementsByClassName('array-bar') // when at animation grab new array bar currently in DOM
+    const isColorChange = i % 3 !== 2; //check to see if dealing with color change (happens first 2 values for every 3)
+    if(isColorChange){
+        const [barOneIndex, barTwoIndex] = animations[i] // dealing with the 2 bars
+        const barOneStyle = arrayBars[barOneIndex].style // changing the bar colors
+        const barTwoStyle = arrayBars[barTwoIndex].style
+        const color = i % 3 === 0 ? 'blue':'lightcoral';
+        setTimeout(() => {
+            barOneStyle.backgroundColor = color
+            barTwoStyle.backgroundColor = color
+        }, i * ANIMATION_SPEED_MS);
+    } else {
+        setTimeout(() => {
+            // at % 3 == 2 dealing with overwriting value; values of animation index of bar we are overwriting and new height we give that bar(new value we are overwriting with)
+            const [barOneIndex, newHeight] = animations[i] 
+            const barOneStyle = arrayBars[barOneIndex].style
+            barOneStyle.height = `${newHeight}px`;
+        }, i  * ANIMATION_SPEED_MS);
+    }
+   }
  }
 
  quickSort() {}
@@ -39,19 +64,18 @@ export default class SortingVisualizer extends React.Component {
 
  bubbleSort() {}
 
- testingSortingAlgorithms() { /* iterates random array between 1 - 1000 then push betwen -1000 and 1000 */
+ /* testingSortingAlgorithms() { /* iterates random array between 1 - 1000 then push betwen -1000 and 1000 
     for (let i = 0; i < 100; i++) {
         const array = []
         const length = randomIntFromInterval(1, 1000);
         for(let i = 0; i < length; i++) {
             array.push(randomIntFromInterval(-1000, 1000));
         }
-        const javaScriptSortedArray = array.slice().sort((a, b) => a - b); /* sorts with js sort */
-        const mergeSortedArray = sortingAlgorithms.mergeSort(array.slice()); /* sorts with merge sort*/
-        console.log(arraysAreEqual(javaScriptSortedArray, mergeSortedArray)); /* logs if they are equal */ 
+        const javaScriptSortedArray = array.slice().sort((a, b) => a - b); //sorts with js sort 
+        const mergeSortedArray = getMergeSortAnimations(array.slice()); //sorts with merge sort
+        console.log(arraysAreEqual(javaScriptSortedArray, mergeSortedArray)); //logs if they are equal 
     }
- }
- 
+ }*/
  render() {
     const {array} = this.state;
 
@@ -62,7 +86,7 @@ export default class SortingVisualizer extends React.Component {
          {array.map((value, idex) => (
             <div className="array-bar" 
                 key={idex}
-                style={{height: `${value}px`}}>
+                style={{backgroundColor: "lightcoral" ,height: `${value}px`}}>
             </div>
          ))}
          <button onClick={() => this.resetArray()}>Generate New Array</button>
@@ -70,7 +94,6 @@ export default class SortingVisualizer extends React.Component {
          <button onClick={() => this.quickSort()}>Quick Sort</button>
          <button onClick={() => this.heapSort()}>Heap Sort</button>
          <button onClick={() => this.bubbleSort()}>Bubble Sort</button>
-         <button onClick={() => this.testingSortingAlgorithms()}>Test Sorting Algorithms</button>
         </div>
     );
  }
@@ -81,10 +104,10 @@ export default class SortingVisualizer extends React.Component {
     return Math.floor(Math.random() * (max - min + 1) + min);
  }
 
- function arraysAreEqual(arrayOne, arrayTwo) { /* check if arrays have same length then check each index, if value dont equal each other return false*/
+ /*function arraysAreEqual(arrayOne, arrayTwo) { /* check if arrays have same length then check each index, if value dont equal each other return false
     if (arrayOne.length !== arrayTwo.length) return false;
     for (let i = 0; i < arrayOne.length; i++) {
         if (arrayOne[i] !== arrayTwo[i]) return false;
     }
     return true;
- }
+ }*/
